70087a74653b82858718e5432e7cf69687e32aca
Yaroslav Ribyak <y4@miritec.com>
2015-04-08 16:31:24 +0300

    student can see his homework and marks

--- profile.php
+++ profile.php
@@ -10,11 +10,16 @@
     <?php
     session_start();
     if (isset($_SESSION['userId'])) {
+        $info = array();
         $userId = $_SESSION['userId'];
         $dbh = new PDO('mysql:host=localhost; dbname=registration; charset=UTF8', 'root', '6710omne8864');
-        $sth = $dbh->prepare("SELECT isTeacher, varFirstName, varSurname, varEmail, varAvatar FROM user WHERE user.intId = :userId");
+        $sth = $dbh->prepare("SELECT isTeacher, varFirstName, varSurname, varEmail, varAvatar, varFileName, intMark FROM user LEFT JOIN homework ON user.intId = homework.intStudentId WHERE user.intId = :userId");
         $sth->execute([':userId' => $userId]);
-        $user = $sth->fetch();
+
+
+        while (($row = $sth->fetch(PDO::FETCH_ASSOC)) !== false) {
+            array_push($info, $row);
+        }
     } else {
         header('Location: /login.php', true, 303);
         exit;
@@ -31,16 +36,33 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-md-4">
-            <p align=right>
-                <img width="200" src="/avatars/<?= $user["varAvatar"] ?>">
-            </p>
+        <div class="col-md-4" align=right>
+            <img width="200" src="/avatars/<?= $info[0]["varAvatar"] ?>">
         </div>
         <div class="col-md-4">
             <p>
-                <b>First name:</b> <?= $user["varFirstName"] ?><br/>
-                <b>Second name:</b> <?= $user["varSurname"] ?><br/>
-                <b>E-mail:</b> <?= $user["varEmail"] ?><br/>
+                <b>First name:</b> <?= $info[0]["varFirstName"] ?><br/>
+                <b>Second name:</b> <?= $info[0]["varSurname"] ?><br/>
+                <b>E-mail:</b> <?= $info[0]["varEmail"] ?><br/>
+
+                <?php
+                if ($info[0]["isTeacher"] != 1) {
+                    print '<table class="table table-striped">';
+                    print '<tr><th>Number</th><th>Link</th><th>Mark</th></tr>';
+                    for ($i = 0; $i < count($info); $i++) {
+                        print "<tr><td>";
+                        print "Homework " . ($i + 1) . ":";
+                        print "</td><td>";
+                        print '<a href="/homework/' . $info[$i]["varFileName"] . '">';
+                        print $info[$i]["varFileName"];
+                        print "</a>";
+                        print "</td><td>";
+                        print $info[$i]["intMark"];
+                        print "</td></tr>";
+                    }
+                }
+                ?>
+                </table>
                 <a href="/logout.php">Log out</a>
             </p>
         </div>
